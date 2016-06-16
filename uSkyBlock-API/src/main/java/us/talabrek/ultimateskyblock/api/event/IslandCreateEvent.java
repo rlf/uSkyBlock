8f79c0f8b280fbbe9df818757155ae8020a13851
Ronald Bunk <Azoraqua@users.noreply.github.com>
2016-06-16 10:47:19 +0200

    IslandCreateEvent created.

---
+++ uSkyBlock-API/src/main/java/us/talabrek/ultimateskyblock/api/event/IslandCreateEvent.java
@@ -0,0 +1,34 @@
+package us.talabrek.ultimateskyblock.api.event; 
+ 
+import org.bukkit.entity.Player; 
+import org.bukkit.event.Event; 
+import org.bukkit.event.HandlerList; 
+import us.talabrek.ultimateskyblock.api.uSkyBlockAPI;
+import us.talabrek.ultimateskyblock.api.IslandInfo; 
+
+public class IslandCreateEvent extends uSkyBlockEvent { 
+   
+    private static final HandlerList handlers = new HandlerList(); 
+ 
+     private final IslandInfo island; 
+ 
+     public IslandCreateEvent(Player player, uSkyBlockAPI api, IslandInfo island) { 
+         super(player, api, Cause.OTHER); 
+         this.island = island; 
+     } 
+
+     @Override 
+    public HandlerList getHandlers() { 
+        return handlers; 
+    } 
+
+ 
+     /** 
+      * Returns the handlers listening to this event. 
+      * Required for Bukkit-events. 
+      * @return the handlers listening to this event. 
+      */ 
+    public static HandlerList getHandlerList() { 
+        return handlers; 
+    } 
+} 
