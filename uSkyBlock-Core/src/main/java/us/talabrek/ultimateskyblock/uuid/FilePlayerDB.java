d15f30cf15c5add0de3de05a6f5a8d4438979b17
Talabrek <ahaffly@gmail.com>
2016-10-11 15:53:56 +0900

    Null Check

--- uSkyBlock-Core/src/main/java/us/talabrek/ultimateskyblock/uuid/FilePlayerDB.java
+++ uSkyBlock-Core/src/main/java/us/talabrek/ultimateskyblock/uuid/FilePlayerDB.java
@@ -60,7 +60,7 @@ public class FilePlayerDB implements PlayerDB {
                     for (String uuid : uuids) {
                         UUID id = UUIDUtil.fromString(uuid);
                         String name = uuid2NameConfig.getString(uuid + ".name", null);
-                        if (name != null) {
+                        if (name != null && id != null) {
                             uuid2nameCache.put(id, name);
                             name2uuidCache.put(name, id);
                             List<String> akas = uuid2NameConfig.getStringList(uuid + ".aka");
