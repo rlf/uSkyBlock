c21f526e484fe30aaf7e5ffbbf5e3ed1497afb2e
rlf <rasmus@lockfuglsang.dk>
2018-07-15 15:01:51 +0200

    committing changes for v2.7.4-201807151501

--- releasenotes.sh
+++ releasenotes.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 tag=$( git describe --tags --abbrev=0 )
 tstamp=$( date +%Y%m%d%H%M )
-tag=${tag%-*}
-git log --oneline ${tag}..HEAD | grep "#" > changelog.txt
+tag="${tag%-*}"
+git log --oneline ${tag}..HEAD | grep "#"
 tag="$tag-$tstamp"
-git commit -a -m "committing changes for $tag"
-git push
-git tag $tag
+#git commit -a -m "committing changes for $tag"
+#git push
+#git tag $tag
 #git push --tags
