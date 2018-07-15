c5317192bda2ed75c5da8496b0f51389b07d5eb6
rlf <rasmus@lockfuglsang.dk>
2018-07-15 15:11:43 +0200

    committing changes for v2.7.4-201807151511

--- prerelease.sh
+++ prerelease.sh
@@ -2,8 +2,12 @@
 tag=$( git describe --tags --abbrev=0 )
 tstamp=$( date +%Y%m%d%H%M )
 tag="${tag%-*}"
-echo "Release notes for ${tag}..${tag}-$tstamp" > changelog.txt
-git log --oneline ${tag}..HEAD | grep "#" >> changelog.txt
+echo "# Change Log for ${tag}..${tag}-$tstamp" > changelog.txt
+git log --oneline ${tag}..HEAD | grep "#" | awk '{printf("  * %s\n",$0)}' >> changelog.txt
+echo "# Translations" >> changelog.txt
+echo "```" >> changelog.txt
+cat uSkyBlock-Plugin/target/gettext-report.txt >> changelog.txt
+echo "```" >> changelog.txt
 tag="$tag-$tstamp"
 git commit -a -m "committing changes for $tag"
 git push
