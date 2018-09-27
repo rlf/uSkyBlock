474a83dc699b15ca4ff92906551f95b19b22d34c
asleeponduty <lukedrong@gmail.com>
2018-09-26 23:29:19 -0500

    Fixed yet another merge miss. I should really not do this while half asleep. TODO: Begin upgrading skyblock gui map code to 1.13

--- uSkyBlock-Core/src/main/java/us/talabrek/ultimateskyblock/handler/MultiverseInventoriesHandler.java
+++ uSkyBlock-Core/src/main/java/us/talabrek/ultimateskyblock/handler/MultiverseInventoriesHandler.java
@@ -1,19 +1,19 @@
-//package us.talabrek.ultimateskyblock.handler;
-//
-//import org.bukkit.Bukkit;
-//import org.bukkit.World;
-//import us.talabrek.ultimateskyblock.handler.multiverseinventories.MultiverseInventoriesAdaptor;
-//
-///**
-// * Handler for accessing Multiverse-Inventories, if enabled.
-// */
-//public enum MultiverseInventoriesHandler {;
-//    public static void linkWorlds(World... worlds) {
-//        if (isMVInv()) {
-//            MultiverseInventoriesAdaptor.linkWorlds(worlds);
-//        }
-//    }
-//    public static boolean isMVInv() {
-//        return Bukkit.getPluginManager().isPluginEnabled("Multiverse-Inventories");
-//    }
-//}
+package us.talabrek.ultimateskyblock.handler;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import us.talabrek.ultimateskyblock.handler.multiverseinventories.MultiverseInventoriesAdaptor;
+
+/**
+ * Handler for accessing Multiverse-Inventories, if enabled.
+ */
+public enum MultiverseInventoriesHandler {;
+    public static void linkWorlds(World... worlds) {
+        if (isMVInv()) {
+            MultiverseInventoriesAdaptor.linkWorlds(worlds);
+        }
+    }
+    public static boolean isMVInv() {
+        return Bukkit.getPluginManager().isPluginEnabled("Multiverse-Inventories");
+    }
+}
