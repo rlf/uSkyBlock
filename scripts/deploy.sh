026725ea488ff1f1e095aa9578d8df69939d1af0
Muspah <43544407+Muspah@users.noreply.github.com>
2019-05-30 23:55:39 +0100

    You always mess something up when working with Travis.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,18 +16,18 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false" ]] && [[ "$TRAVIS_BRANCH" == "master" ]]
 
   echo -e "Publishing javadocs...\n"
 
-  rsync -r --delete --quiet $HOME/build/uskyblock/uSkyBlock-API/target/site/apidocs/ \
+  rsync -r --delete --quiet $HOME/build/uskyblock/uSkyBlock/uSkyBlock-API/target/site/apidocs/ \
   dool1@shell.xs4all.nl:WWW/javadocs/master/uSkyBlock-API/
 
-  rsync -r --delete --quiet $HOME/build/uskyblock/uSkyBlock-Core/target/site/apidocs/ \
+  rsync -r --delete --quiet $HOME/build/uskyblock/uSkyBlock/uSkyBlock-Core/target/site/apidocs/ \
   dool1@shell.xs4all.nl:WWW/javadocs/master/uSkyBlock-Core/
 
-  rsync -r --delete --quiet $HOME/build/uskyblock/uSkyBlock-FAWE/target/site/apidocs/ \
+  rsync -r --delete --quiet $HOME/build/uskyblock/uSkyBlock/uSkyBlock-FAWE/target/site/apidocs/ \
   dool1@shell.xs4all.nl:WWW/javadocs/master/uSkyBlock-FAWE/
 
   echo -e "Publishing final plugin release...\n"
 
-  rsync -r --quiet $HOME/build/uskyblock/uSkyBlock-Plugin/target/uSkyBlock-*.jar \
+  rsync -r --quiet $HOME/build/uskyblock/uSkyBlock/uSkyBlock-Plugin/target/uSkyBlock-*.jar \
   dool1@shell.xs4all.nl:WWW/downloads/master/uSkyBlock/
 
 fi
@@ -48,18 +48,18 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false" ]] && [[ "$TRAVIS_BRANCH" == "release" ]
 
   echo -e "Publishing javadocs...\n"
 
-  rsync -r --delete --quiet $HOME/build/uskyblock/uSkyBlock-API/target/site/apidocs/ \
+  rsync -r --delete --quiet $HOME/build/uskyblock/uSkyBlock/uSkyBlock-API/target/site/apidocs/ \
   dool1@shell.xs4all.nl:WWW/javadocs/release/uSkyBlock-API/
 
-  rsync -r --delete --quiet $HOME/build/uskyblock/uSkyBlock-Core/target/site/apidocs/ \
+  rsync -r --delete --quiet $HOME/build/uskyblock/uSkyBlock/uSkyBlock-Core/target/site/apidocs/ \
   dool1@shell.xs4all.nl:WWW/javadocs/release/uSkyBlock-Core/
 
-  rsync -r --delete --quiet $HOME/build/uskyblock/uSkyBlock-FAWE/target/site/apidocs/ \
+  rsync -r --delete --quiet $HOME/build/uskyblock/uSkyBlock/uSkyBlock-FAWE/target/site/apidocs/ \
   dool1@shell.xs4all.nl:WWW/javadocs/release/uSkyBlock-FAWE/
 
   echo -e "Publishing final plugin release...\n"
 
-  rsync -r --quiet $HOME/build/uskyblock/uSkyBlock-Plugin/target/uSkyBlock-*.jar \
+  rsync -r --quiet $HOME/build/uskyblock/uSkyBlock/uSkyBlock-Plugin/target/uSkyBlock-*.jar \
   dool1@shell.xs4all.nl:WWW/downloads/release/uSkyBlock/
 
 fi
