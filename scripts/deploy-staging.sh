39e540b7a22ee106d65317e754d3a5ecd41f77d3
Muspah <43544407+Muspah@users.noreply.github.com>
2019-07-12 23:05:17 +0100

    Please Travis, it's friday?

---
+++ scripts/deploy-staging.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+
+    echo -e "Running staging script...\n"
+    echo -e "Publishing artifacts...\n"
+    cd $HOME
+
+    rsync -r --quiet $HOME/build/uskyblock/uSkyBlock/uSkyBlock-API/target/mvn-repo/ \
+    dool1@shell.xs4all.nl:WWW/maven/uskyblock/
+
+    rsync -r --quiet $HOME/build/uskyblock/uSkyBlock/uSkyBlock-Core/target/mvn-repo/ \
+    dool1@shell.xs4all.nl:WWW/maven/uskyblock/
+
+    rsync -r --quiet $HOME/build/uskyblock/uSkyBlock/uSkyBlock-FAWE/target/mvn-repo/ \
+    dool1@shell.xs4all.nl:WWW/maven/uskyblock/
+
+    echo -e "Publishing javadocs...\n"
+
+    rsync -r --delete --quiet $HOME/build/uskyblock/uSkyBlock/uSkyBlock-API/target/site/apidocs/ \
+    dool1@shell.xs4all.nl:WWW/javadocs/master/uSkyBlock-API/
+
+    rsync -r --delete --quiet $HOME/build/uskyblock/uSkyBlock/uSkyBlock-Core/target/site/apidocs/ \
+    dool1@shell.xs4all.nl:WWW/javadocs/master/uSkyBlock-Core/
+
+    rsync -r --delete --quiet $HOME/build/uskyblock/uSkyBlock/uSkyBlock-FAWE/target/site/apidocs/ \
+    dool1@shell.xs4all.nl:WWW/javadocs/master/uSkyBlock-FAWE/
+
+    echo -e "Publishing final plugin release...\n"
+
+    rsync -r --quiet $HOME/build/uskyblock/uSkyBlock/uSkyBlock-Plugin/target/uSkyBlock-*.jar \
+    dool1@shell.xs4all.nl:WWW/downloads/master/uSkyBlock/
+
+fi
