dbb3590cf82eb826be2f34e85f40d171b04ce716
Muspah <43544407+Muspah@users.noreply.github.com>
2019-09-17 14:49:35 +0100

    Add po-utils to travis deploy scripts

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -3,8 +3,11 @@
 if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
 
     echo -e "Running staging script...\n"
-    echo -e "Publishing artifacts...\n"
+    echo -e "Publishing javadocs and artifacts...\n"
     cd $HOME
+
+    rsync -r --quiet $HOME/build/uskyblock/uSkyBlock/po-utils/target/mvn-repo/ \
+    dool1@shell.xs4all.nl:WWW/maven/uskyblock/
 
     rsync -r --quiet $HOME/build/uskyblock/uSkyBlock/uSkyBlock-API/target/mvn-repo/ \
     dool1@shell.xs4all.nl:WWW/maven/uskyblock/
@@ -16,6 +19,9 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     dool1@shell.xs4all.nl:WWW/maven/uskyblock/
 
     echo -e "Publishing javadocs...\n"
+
+    rsync -r --delete --quiet $HOME/build/uskyblock/uSkyBlock/po-utils/target/site/apidocs/ \
+    dool1@shell.xs4all.nl:WWW/javadocs/master/po-utils/
 
     rsync -r --delete --quiet $HOME/build/uskyblock/uSkyBlock/uSkyBlock-API/target/site/apidocs/ \
     dool1@shell.xs4all.nl:WWW/javadocs/master/uSkyBlock-API/
