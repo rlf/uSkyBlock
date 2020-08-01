00cb3712b5af633851d0117f1b30dd222ac7e351
Muspah <simondw@sapo.pt>
2020-08-01 20:53:44 +0100

    Add AWE370 to build scripts (please Travis, I always break something here, be nice for me tonight)

--- scripts/deploy-release.sh
+++ scripts/deploy-release.sh
@@ -6,35 +6,44 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
   echo -e "Publishing javadocs and artifacts...\n"
   cd $HOME
 
-  rsync -r --quiet $HOME/build/uskyblock/uSkyBlock/po-utils/target/mvn-repo/ \
-  dool1@shell.xs4all.nl:WWW/maven/uskyblock/
+  rsync -r --quiet "ssh -p 2222 -o StrictHostKeyChecking=no" \
+  $HOME/build/uskyblock/uSkyBlock/po-utils/target/mvn-repo/ \
+  travis@travis.internetpolice.eu:WWW-USB/maven/uskyblock/
 
-  rsync -r --quiet $HOME/build/uskyblock/uSkyBlock/uSkyBlock-API/target/mvn-repo/ \
-  dool1@shell.xs4all.nl:WWW/maven/uskyblock/
+  rsync -r --quiet "ssh -p 2222 -o StrictHostKeyChecking=no" \
+  $HOME/build/uskyblock/uSkyBlock/uSkyBlock-API/target/mvn-repo/ \
+  travis@travis.internetpolice.eu:WWW-USB/maven/uskyblock/
 
-  rsync -r --quiet $HOME/build/uskyblock/uSkyBlock/uSkyBlock-Core/target/mvn-repo/ \
-  dool1@shell.xs4all.nl:WWW/maven/uskyblock/
+  rsync -r --quiet "ssh -p 2222 -o StrictHostKeyChecking=no" \
+  $HOME/build/uskyblock/uSkyBlock/uSkyBlock-Core/target/mvn-repo/ \
+  travis@travis.internetpolice.eu:WWW-USB/maven/uskyblock/
 
-  rsync -r --quiet $HOME/build/uskyblock/uSkyBlock/uSkyBlock-FAWE/target/mvn-repo/ \
-  dool1@shell.xs4all.nl:WWW/maven/uskyblock/
+  rsync -r --quiet "ssh -p 2222 -o StrictHostKeyChecking=no" \
+  $HOME/build/uskyblock/uSkyBlock/uSkyBlock-FAWE/target/mvn-repo/ \
+  travis@travis.internetpolice.eu:WWW-USB/maven/uskyblock/
+
+  rsync -r --quiet "ssh -p 2222 -o StrictHostKeyChecking=no" \
+  $HOME/build/uskyblock/uSkyBlock/uSkyBlock-AWE370/target/mvn-repo/ \
+  travis@travis.internetpolice.eu:WWW-USB/maven/uskyblock/
 
   echo -e "Publishing javadocs...\n"
 
-  rsync -r --delete --quiet $HOME/build/uskyblock/uSkyBlock/po-utils/target/site/apidocs/ \
-  dool1@shell.xs4all.nl:WWW/javadocs/release/po-utils/
+  rsync -r --delete --quiet "ssh -p 2222 -o StrictHostKeyChecking=no" \
+  $HOME/build/uskyblock/uSkyBlock/po-utils/target/site/apidocs/ \
+  travis@travis.internetpolice.eu:WWW-USB/javadocs/release/po-utils/
 
-  rsync -r --delete --quiet $HOME/build/uskyblock/uSkyBlock/uSkyBlock-API/target/site/apidocs/ \
-  dool1@shell.xs4all.nl:WWW/javadocs/release/uSkyBlock-API/
+  rsync -r --delete --quiet "ssh -p 2222 -o StrictHostKeyChecking=no" \
+  $HOME/build/uskyblock/uSkyBlock/uSkyBlock-API/target/site/apidocs/ \
+  travis@travis.internetpolice.eu:WWW-USB/javadocs/release/uSkyBlock-API/
 
-  rsync -r --delete --quiet $HOME/build/uskyblock/uSkyBlock/uSkyBlock-Core/target/site/apidocs/ \
-  dool1@shell.xs4all.nl:WWW/javadocs/release/uSkyBlock-Core/
-
-  rsync -r --delete --quiet $HOME/build/uskyblock/uSkyBlock/uSkyBlock-FAWE/target/site/apidocs/ \
-  dool1@shell.xs4all.nl:WWW/javadocs/release/uSkyBlock-FAWE/
+  rsync -r --delete --quiet "ssh -p 2222 -o StrictHostKeyChecking=no" \
+  $HOME/build/uskyblock/uSkyBlock/uSkyBlock-Core/target/site/apidocs/ \
+  travis@travis.internetpolice.eu:WWW-USB/javadocs/release/uSkyBlock-Core/
 
   echo -e "Publishing final plugin release...\n"
 
-  rsync -r --quiet $HOME/build/uskyblock/uSkyBlock/uSkyBlock-Plugin/target/uSkyBlock-*.jar \
-  dool1@shell.xs4all.nl:WWW/downloads/release/uSkyBlock/
+  rsync -r --quiet "ssh -p 2222 -o StrictHostKeyChecking=no" \
+  $HOME/build/uskyblock/uSkyBlock/uSkyBlock-Plugin/target/uSkyBlock-*.jar \
+  travis@travis.internetpolice.eu:WWW-USB/downloads/release/uSkyBlock/
 
 fi
