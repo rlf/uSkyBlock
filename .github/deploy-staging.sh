9258bb4b016670f1848444ab3fb5794637cfa727
Muspah <simondw@sapo.pt>
2021-12-21 14:22:02 +0000

    GH Actions deploy test #1

---
+++ .github/deploy-staging.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+echo -e "Running staging script...\n"
+echo -e "Publishing javadocs and artifacts...\n"
+cd $HOME
+
+rsync -r --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
+$HOME/work/uskyblock/uSkyBlock/po-utils/target/mvn-repo/ \
+travis@travis.internetpolice.eu:WWW-USB/maven/uskyblock/
+
+rsync -r --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
+$HOME/work/uskyblock/uSkyBlock/target/mvn-repo/ \
+travis@travis.internetpolice.eu:WWW-USB/maven/uskyblock/
+
+rsync -r --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
+$HOME/work/uskyblock/uSkyBlock/uSkyBlock-API/target/mvn-repo/ \
+travis@travis.internetpolice.eu:WWW-USB/maven/uskyblock/
+
+rsync -r --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
+$HOME/work/uskyblock/uSkyBlock/uSkyBlock-AWE370/target/mvn-repo/ \
+travis@travis.internetpolice.eu:WWW-USB/maven/uskyblock/
+
+rsync -r --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
+$HOME/work/uskyblock/uSkyBlock/uSkyBlock-Core/target/mvn-repo/ \
+travis@travis.internetpolice.eu:WWW-USB/maven/uskyblock/
+
+rsync -r --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
+$HOME/work/uskyblock/uSkyBlock/uSkyBlock-FAWE/target/mvn-repo/ \
+travis@travis.internetpolice.eu:WWW-USB/maven/uskyblock/
+
+rsync -r --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
+$HOME/work/uskyblock/uSkyBlock/uSkyBlock-Plugin/target/mvn-repo/ \
+travis@travis.internetpolice.eu:WWW-USB/maven/uskyblock/
+
+echo -e "Publishing javadocs...\n"
+
+rsync -r --delete --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
+$HOME/work/uskyblock/uSkyBlock/po-utils/target/site/apidocs/ \
+travis@travis.internetpolice.eu:WWW-USB/javadocs/master/po-utils/
+
+rsync -r --delete --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
+$HOME/work/uskyblock/uSkyBlock/uSkyBlock-API/target/site/apidocs/ \
+travis@travis.internetpolice.eu:WWW-USB/javadocs/master/uSkyBlock-API/
+
+rsync -r --delete --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
+$HOME/work/uskyblock/uSkyBlock/uSkyBlock-Core/target/site/apidocs/ \
+travis@travis.internetpolice.eu:WWW-USB/javadocs/master/uSkyBlock-Core/
+
+echo -e "Publishing final plugin release...\n"
+
+rsync -r --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
+$HOME/work/uskyblock/uSkyBlock/uSkyBlock-Plugin/target/uSkyBlock-*.jar \
+travis@travis.internetpolice.eu:WWW-USB/downloads/master/uSkyBlock/
